418f2159906511c737b52861cef8b90e18046112
Bob Lee <b0b@steelguitarnews.com>
2016-03-20 23:26:50 -0700

    additional constants for the SN-U110-10 Rock Drums sound card

--- lib/drums110.rb
+++ lib/drums110.rb
@@ -15,7 +15,7 @@
 # :ap ride    RCM  z$ ;ap  ' ride  is Ride
 
 module Drums110
-# terse mnemonics for each drum in the kit
+  # terse constants for each drum in the internal U-110 kit
   BD1 = 35    # bass drum 1
   BD2 = 36    # bass drum 2
   RS = 37     # rim shot
@@ -64,4 +64,38 @@ module Drums110
     56, 57, 58, 59, 69, 79, 81,
     84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94
   ]
+
+  # additional constants for the SN-U110-10 "Rock Drums" sound card
+  R_BD1 = 35    # Bass Drum 1
+  R_BD2 = 36    # Bass Drum 2
+  R_RS = 37     # Rim Shot
+  R_SD1 = 38    # Snare Drum 1
+  R_STICK = 39  # Stick
+  R_SD2 = 40    # Snare Drum 2
+  R_LT1 = 41    # Low Tom 1
+  R_CH1 = 42    # Closed High Hat
+  R_LT2 = 43    # Low Tom 2
+  R_HOH1 = 44   # Half Open High Hat 1
+  R_MT1 = 45    # Middle Tom 1
+  R_OH1 = 46    # Open High Hat 1
+  R_MT2 = 47    # Middle Tom 2
+  R_HT1 = 48    # High Tom 1
+  R_CC = 49     # Crash Cymbal
+  R_HT2 = 50    # High Tom 2
+  R_CUP = 51    # Cup
+  R_GONG = 52   # Gong
+  R_CC2 = 57    # Crash Cymbal
+  R_SD3 = 58    # Snare Drum 3
+  R_HPT2 = 79   # High Pitched Tom 2
+  R_HPT1 = 81   # High Pitched Tom 1
+  R_BD3 = 84    # Bass Drum 3
+  R_BD4 = 85    # Bass Drum 4
+  R_SD4 = 86    # Snare Drum 4
+  R_SD5 = 87    # Snare Drum 5
+  R_SD6 = 88    # Snare Drum 6
+  R_LT3 = 89    # Low Tom 3
+  R_CH2 = 90    # Closed High Hat
+  R_MT3 = 91    # Middle Tom 3
+  R_CC3 = 92    # Crash Cymbal
+  R_HT3 = 93    # High Tom 3
 end
